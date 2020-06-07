25547cc4ce6fa591380e6d8f44068179322dec01
Tran Kim Nikoletta <trankimniki@gmail.com>
2020-06-07 15:36:56 +0200

    exercise 2 done

--- exercise-2/script.js
+++ exercise-2/script.js
@@ -4,7 +4,18 @@ let word = 'cool';
 
 if (color == 'purple') {
     $('.north').css('background-color', 'purple')
+};
+
+$('.east').text('East2');
+
+if (number>100) {
+    console.log("whoah, that's a big number.")
+} else {
+    console.log("just a regular number, please.")
+};
+
+if (word === 'cool') {
+    $('.south').text('Power of DOM')
+} else {
+    $('.west').text('Power of DOM')
 }
-
-$('.east').text('East2')
-
